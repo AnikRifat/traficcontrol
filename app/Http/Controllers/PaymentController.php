4ecb882e42774e91779aca37803f939a4f8933f5
Anik Rifat <reafatul@gmail.com>
2023-10-31 20:39:12 +0600

    UPDATE

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
+use PayMoney\Api\Amount;
+use PayMoney\Api\Payer;
+use PayMoney\Api\Payment;
+use PayMoney\Api\RedirectUrls;
+use PayMoney\Api\Transaction;
+
 
 class PaymentController extends Controller
 {
-    //
+    public function pay(Request $request)
+    {
+
+        $data = $request->all();
+        $payer = new Payer();
+        $payer->setPaymentMethod('PayMoney');
+        $amountIns = new Amount();
+        $amountIns->setTotal($data['grand_total'])->setCurrency($currency_code = Currency::select('code')->find($data['currency_id'])->code);
+
+        $trans = new Transaction();
+        $trans->setAmount($amountIns);
+
+        $urls = new RedirectUrls();
+        $urls->setReturnUrl(url('/admin/reports')->alert('Paid !', 'success'))
+            ->setCancelUrl(url('/admin/reports')->alert('Canceled !', 'warning'));
+
+        $payment = new Payment();
+        $payment->setCredentials([
+            'client_id' => env('PAYMONEY_CLIENTID'),
+            'client_secret' => env('PAYMONEY_CLIENTSECRET')
+        ])->setRedirectUrls($urls)
+            ->setPayer($payer)
+            ->setTransaction($trans);
+
+        try {
+            $payment->create();
+            $url = $payment->getApprovedUrl();
+            return redirect($url);
+        } catch (Exception $ex) {
+            print $ex;
+            exit;
+            return redirect()->back()->alert('Payment Unsuccessfull !', 'warning');
+        }
+    }
 }
